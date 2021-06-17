bb6788b166e85d7782f82a76dc41eea8113df031
SandraNasasiira <79199579+SandraNasasiira@users.noreply.github.com>
2021-06-17 11:18:15 +0300

    Add files via upload

---
+++ python_test.py
@@ -0,0 +1,55 @@
+x = [100,110,120,130,140,150]
+p=[]
+for i in x:
+    a=i*5
+    p.append(a)
+    print(p)
+
+def divisible_by_three(v):
+    g=range(v+1)
+    x=[]
+    for i in x:
+        if i%3==0:
+            x.append(u)
+    print(x)
+divisible_by_three(56)
+
+
+
+x = [[1,2],[3,4],[5,6]]
+def new_list():
+      y=[]
+    print(y(x))
+
+def smallest(list):
+    num = list[0]
+    for x in list:
+        if x<num:
+print(smallest([3,6,8,4,1,5,7]))
+
+def remove_duplicate():
+    x = ['a','b','a','e','d','b','c','e','f','g','h']
+    x=sorted(set(x))
+    return x
+print(remove_duplicate())
+
+def divisible_by_seven():
+        y=range(100,200)
+    for x in y:
+        if x%7==0:
+            print(x)
+
+divisible_by_seven()
+
+
+def  greet_student(self,name,year,age):
+    self.age=age
+    self.name=name
+    self.year=year
+    greet_student(self,name,year,age)
+        return f"hello {self.name},you were born in{self.year}and you are{self.age}old."
+
+        greet_student("sandra",2000,21)
+        greet_student("Promise",2001,20)
+        greet_student("Mary",2019,3)
+
